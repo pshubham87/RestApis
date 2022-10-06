501d14e212e9cee863babce8aed1ea334987ab65
pshubham87 <patil.shubham.consultant@mgstech.in>
2022-10-06 14:31:32 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,23 +5,20 @@ import 'package:restapi/LogIn.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  dynamic token = SharedPreferences.getInstance();
-
   MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        // token != '' ? : const HomeScreen()
-        home: const MyHomePage());
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
   }
 }
