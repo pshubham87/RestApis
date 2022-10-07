b1eea383599b21f540c07cbaef6ca51ae8f16796
pshubham87 <patil.shubham.consultant@mgstech.in>
2022-10-07 18:03:54 +0530

    comitt

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,29 @@ import 'package:restapi/HomeScreen.dart';
 import 'package:restapi/LogIn.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences pref = await SharedPreferences.getInstance();
+  var profiletoken = pref.getString('token') ?? null;
+  runApp(MaterialApp(
+    home: profiletoken == null ? const MyHomePage() : const HomeScreen(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
+// class MyApp extends StatelessWidget {
+
+
+
+//   MyApp({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: token != '' ? const MyHomePage() : const HomeScreen(),
+//     );
+//   }
+// }
